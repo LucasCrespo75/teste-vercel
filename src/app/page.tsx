98c4fc3ef1e633f87b9625a4d0bca2f2cb55a1f0
LucasCrespo75 <lucas.crespo@mv.com.br>
2025-06-16 17:35:16 -0300

    feat add: sessoes servicos, equipes e obras

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Header from "./component/Header/HeaderHome";
 import SobreSession from "./component/SobreSessionHome/SobreSession";
+import Servico from "./component/Servico/Servico";
+import Equipe from "./component/MembrosObras/Equipe";
 
 export default function Home() {
   return (
     <>
       <Header />
       <SobreSession />
+      <Servico />
+      <Equipe />
     </>
   );
 }
